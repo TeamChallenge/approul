0df8c2828804b75d943912bea1b60df55f466af3
Jerlilson Bezerra da Silva <jerlilsonbs@icloud.com>
2017-02-09 17:59:09 -0400

    Classe de animação
    Signed-off-by: Jerlilson Bezerra da Silva <jerlilsonbs@icloud.com>

---
+++ Roul/ModalDuelo.swift
@@ -0,0 +1,21 @@
+//
+//  ModalDuelo.swift
+//  Roul
+//
+//  Created by Renata da Silva Nunes on 09/02/17.
+//  Copyright © 2017 Jerlilson Bezerra da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ModalDuelo: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
