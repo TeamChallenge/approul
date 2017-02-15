34829c8070a1a7613379585433bf5e59d8e78adf
Renata da Silva Nunes <Renata@Renata-MacBook-Pro.local>
2017-02-15 17:26:50 -0400

    Criacao da classe para controle da view que contém os avatares dos 2 jogadores que irão jogar
    Signed-off-by: Renata da Silva Nunes <Renata@Renata-MacBook-Pro.local>

---
+++ Roul/ViewJogadores.swift
@@ -0,0 +1,21 @@
+//
+//  ViewJogadores.swift
+//  Roul
+//
+//  Created by Renata da Silva Nunes on 15/02/17.
+//  Copyright © 2017 Jerlilson Bezerra da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewJogadores: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
